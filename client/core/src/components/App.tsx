6714c70c48c6c61e3d5593ba4c9e5d2ab95eb969
jaycammarano <jaycammarano@gmail.com>
2021-05-02 16:53:37 -0400

    Created bio page and linked from app

--- client/core/src/components/App.tsx
+++ client/core/src/components/App.tsx
@@ -1,12 +1,14 @@
 import '../assets/css/styles.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './HomePage/HomePage';
+import UserProfile from './UserProfile/UserProfile';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/user/0" component={UserProfile} />
       </Switch>
     </BrowserRouter>
   );
