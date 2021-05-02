88f24c2dbe8b2e09a329bc8bf70034f9fe731445
jaycammarano <jaycammarano@gmail.com>
2021-05-02 17:51:43 -0400

    fixed bug where page doesnt go past 1 screen

--- client/core/src/components/Layout/Layout.tsx
+++ client/core/src/components/Layout/Layout.tsx
@@ -4,34 +4,34 @@ import { ILayout } from './interfaces';
 const Layout: React.FC<ILayout> = ({ children }: ILayout) => {
   return (
     <>
-      <div className="" style={{}}>
+      <div className="bg-gray-100" style={{}}>
         <div
-          className="h-screen flex bg-gray-100"
+          className="flex min-h-screen bg-gray-100"
           style={{ minHeight: '640px' }}
         >
           <div className="flex flex-col w-64">
-            <div className="flex flex-col h-0 flex-1 border-r border-gray-200 bg-white">
-              <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
+            <div className="flex flex-col flex-1 h-0 bg-white border-r border-gray-200">
+              <div className="flex flex-col flex-1 pt-5 pb-4 overflow-y-auto">
                 <div className="flex items-center flex-shrink-0 px-4">
                   <img
-                    className="h-8 w-auto"
+                    className="w-auto h-8"
                     src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
                     alt="Workflow"
                   />
                 </div>
                 <nav
-                  className="mt-5 flex-1 px-2 bg-white space-y-1"
+                  className="flex-1 px-2 mt-5 space-y-1 bg-white"
                   aria-label="Sidebar"
                 >
                   <a
                     href="/"
-                    className="bg-gray-100 text-gray-900 hover:text-gray-900 hover:bg-gray-100 group flex items-center px-2 py-2 text-sm font-medium rounded-md"
+                    className="flex items-center px-2 py-2 text-sm font-medium text-gray-900 bg-gray-100 rounded-md hover:text-gray-900 hover:bg-gray-100 group"
                     data-todo-x-state-on="Current"
                     data-todo-x-state-off="Default"
                     data-todo-x-state-description='Current: "bg-gray-100 text-gray-900 hover:text-gray-900 hover:bg-gray-100", Default: "text-gray-600 hover:text-gray-900 hover:bg-gray-50"'
                   >
                     <svg
-                      className="text-gray-500 mr-3 h-6 w-6"
+                      className="w-6 h-6 mr-3 text-gray-500"
                       data-todo-x-state-on="Current"
                       data-todo-x-state-off="Default"
                       data-todo-x-state-description='Current: "text-gray-500", Default: "text-gray-400 group-hover:text-gray-500"'
@@ -53,11 +53,11 @@ const Layout: React.FC<ILayout> = ({ children }: ILayout) => {
                   </a>
                   <Link
                     to="/user/0"
-                    className="text-gray-600 hover:text-gray-900 hover:bg-gray-50 group flex items-center px-2 py-2 text-sm font-medium rounded-md"
+                    className="flex items-center px-2 py-2 text-sm font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50 group"
                     data-todo-x-state-description='undefined: "bg-gray-100 text-gray-900 hover:text-gray-900 hover:bg-gray-100", undefined: "text-gray-600 hover:text-gray-900 hover:bg-gray-50"'
                   >
                     <svg
-                      className="text-gray-400 group-hover:text-gray-500 mr-3 h-6 w-6"
+                      className="w-6 h-6 mr-3 text-gray-400 group-hover:text-gray-500"
                       data-todo-x-state-description='undefined: "text-gray-500", undefined: "text-gray-400 group-hover:text-gray-500"'
                       data-todo-x-description="Heroicon name: outline/users"
                       xmlns="http://www.w3.org/2000/svg"
@@ -75,7 +75,7 @@ const Layout: React.FC<ILayout> = ({ children }: ILayout) => {
                     </svg>
                     Profile
                     <Link
-                      className="bg-gray-100 group-hover:bg-gray-200 ml-auto inline-block text-xs font-medium rounded-full"
+                      className="inline-block ml-auto text-xs font-medium bg-gray-100 rounded-full group-hover:bg-gray-200"
                       to="user/profile/edit"
                     >
                       <span
@@ -90,11 +90,11 @@ const Layout: React.FC<ILayout> = ({ children }: ILayout) => {
                   </Link>{' '}
                   <a
                     href="/"
-                    className="text-gray-600 hover:text-gray-900 hover:bg-gray-50 group flex items-center px-2 py-2 text-sm font-medium rounded-md"
+                    className="flex items-center px-2 py-2 text-sm font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50 group"
                     data-todo-x-state-description='undefined: "bg-gray-100 text-gray-900 hover:text-gray-900 hover:bg-gray-100", undefined: "text-gray-600 hover:text-gray-900 hover:bg-gray-50"'
                   >
                     <svg
-                      className="text-gray-400 group-hover:text-gray-500 mr-3 h-6 w-6"
+                      className="w-6 h-6 mr-3 text-gray-400 group-hover:text-gray-500"
                       data-todo-x-state-description='undefined: "text-gray-500", undefined: "text-gray-400 group-hover:text-gray-500"'
                       data-todo-x-description="Heroicon name: outline/users"
                       xmlns="http://www.w3.org/2000/svg"
@@ -122,11 +122,11 @@ const Layout: React.FC<ILayout> = ({ children }: ILayout) => {
                   </a>
                   <a
                     href="/"
-                    className="text-gray-600 hover:text-gray-900 hover:bg-gray-50 group flex items-center px-2 py-2 text-sm font-medium rounded-md"
+                    className="flex items-center px-2 py-2 text-sm font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50 group"
                     data-todo-x-state-description='undefined: "bg-gray-100 text-gray-900 hover:text-gray-900 hover:bg-gray-100", undefined: "text-gray-600 hover:text-gray-900 hover:bg-gray-50"'
                   >
                     <svg
-                      className="text-gray-400 group-hover:text-gray-500 mr-3 h-6 w-6"
+                      className="w-6 h-6 mr-3 text-gray-400 group-hover:text-gray-500"
                       data-todo-x-state-description='undefined: "text-gray-500", undefined: "text-gray-400 group-hover:text-gray-500"'
                       data-todo-x-description="Heroicon name: outline/folder"
                       xmlns="http://www.w3.org/2000/svg"
@@ -152,11 +152,11 @@ const Layout: React.FC<ILayout> = ({ children }: ILayout) => {
                   </a>
                   <a
                     href="/"
-                    className="text-gray-600 hover:text-gray-900 hover:bg-gray-50 group flex items-center px-2 py-2 text-sm font-medium rounded-md"
+                    className="flex items-center px-2 py-2 text-sm font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50 group"
                     data-todo-x-state-description='undefined: "bg-gray-100 text-gray-900 hover:text-gray-900 hover:bg-gray-100", undefined: "text-gray-600 hover:text-gray-900 hover:bg-gray-50"'
                   >
                     <svg
-                      className="text-gray-400 group-hover:text-gray-500 mr-3 h-6 w-6"
+                      className="w-6 h-6 mr-3 text-gray-400 group-hover:text-gray-500"
                       data-todo-x-state-description='undefined: "text-gray-500", undefined: "text-gray-400 group-hover:text-gray-500"'
                       data-todo-x-description="Heroicon name: outline/calendar"
                       xmlns="http://www.w3.org/2000/svg"
