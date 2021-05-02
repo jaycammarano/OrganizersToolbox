27d1ecb39638290d8b520c0e7cf8c72a0735ff30
jaycammarano <jaycammarano@gmail.com>
2021-05-02 18:11:30 -0400

    SignUp component and route made

--- client/core/src/components/App.tsx
+++ client/core/src/components/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './HomePage/HomePage';
 import UserProfile from './UserProfile/UserProfile';
 import OrgProfile from './OrgProfile/OrgProfile';
+import UserSignUp from './UserSignUp/UserSignUp';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/signup" component={UserSignUp} />
         <Route exact path="/user/0" component={UserProfile} />
         <Route exact path="/org/0" component={OrgProfile} />
       </Switch>
---
+++ client/core/src/components/UserSignUp/UserSignUp.tsx
@@ -0,0 +1,5 @@
+const UserSignUp: React.FC = () => {
+  return <div />;
+};
+
+export default UserSignUp;
