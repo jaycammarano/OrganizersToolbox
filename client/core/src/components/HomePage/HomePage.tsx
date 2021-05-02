f02b209f1d89dfb39c98fea42920dd1463e07590
jaycammarano <jaycammarano@gmail.com>
2021-05-02 15:14:30 -0400

    cards made, info hardcoded though

--- client/core/src/components/HomePage/HomePage.tsx
+++ client/core/src/components/HomePage/HomePage.tsx
@@ -1,7 +1,27 @@
 import Layout from '../Layout/Layout';
+import UserCard from './UserCard';
+import OrganizationCard from './OrganizationCard';
 
 const HomePage: React.FC = () => {
-  return <Layout>Hello</Layout>;
+  return (
+    <Layout>
+      <UserCard
+        name="Jay Cammarano"
+        icon="https://avatars.githubusercontent.com/u/67079013?v=4"
+        tagLine="Production Manager"
+      />
+      <div className="px-8">
+        <h2 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+          Organizations:
+        </h2>
+      </div>
+      <OrganizationCard
+        name="MCDPC"
+        tagLine="Meeting drug use with compassion not fear."
+        icon="https://avatars.githubusercontent.com/u/67079013?v=4"
+      />
+    </Layout>
+  );
 };
 
 export default HomePage;
---
+++ client/core/src/components/HomePage/OrganizationCard.tsx
@@ -0,0 +1,60 @@
+import { ICard } from './interfaces';
+
+const OrganizationCard: React.FC<ICard> = ({ name, icon, tagLine }: ICard) => {
+  return (
+    <div className="py-8">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="rounded-lg bg-white overflow-hidden shadow">
+          <div className="bg-white p-6">
+            <div className="sm:flex sm:items-center sm:justify-between">
+              <div className="sm:flex sm:space-x-5">
+                <div className="flex-shrink-0">
+                  <img
+                    className="mx-auto h-20 w-20 rounded-full"
+                    src={icon}
+                    alt=""
+                  />
+                </div>
+                <div className="mt-4 text-center sm:mt-0 sm:pt-1 sm:text-left">
+                  <p className="text-xl font-bold text-gray-900 sm:text-2xl">
+                    {name}
+                  </p>
+                  <p className="text-sm font-medium text-gray-600">{tagLine}</p>
+                </div>
+              </div>
+              <div className="mt-5 flex justify-center sm:mt-0">
+                <a
+                  href="/"
+                  className="flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+                >
+                  View profile
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="border-t border-gray-200 bg-gray-50 grid grid-cols-1 divide-y divide-gray-200 sm:grid-cols-3 sm:divide-y-0 sm:divide-x">
+            <div className="px-6 py-5 text-sm font-medium text-center">
+              <span className="text-gray-900">12</span>
+              {/* space */}
+              <span className="text-gray-600"> Upcoming Tasks</span>
+            </div>
+
+            <div className="px-6 py-5 text-sm font-medium text-center">
+              <span className="text-gray-900">4</span>
+              {/* space */}
+              <span className="text-gray-600"> Tasks Due Today</span>
+            </div>
+
+            <div className="px-6 py-5 text-sm font-medium text-center">
+              <span className="text-gray-900">2</span>
+              {/* space */}
+              <span className="text-gray-600"> Overdue Tasks</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OrganizationCard;
